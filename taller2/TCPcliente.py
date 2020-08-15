da158c1d70ca4f0e1b39b682526441ae12c66fb9
Felipe <guzman2000f@gmail.com>
2020-08-14 19:38:01 -0500

    nuevo cliente

--- taller2/TCPcliente.py
+++ taller2/TCPcliente.py
@@ -1,11 +1,60 @@
 from socket import *
 
 servidorNombre = "127.0.0.1" 
-servidorPuerto = 12000
+servidorPuerto = 12006
+
+
+# Solicitar saldo
 clienteSocket = socket(AF_INET, SOCK_STREAM)
 clienteSocket.connect((servidorNombre,servidorPuerto))
-mensaje = input("Ingrese un mensaje:")
+mensaje = "saldo"
 clienteSocket.send(bytes(mensaje, "utf-8"))
 mensajeRespuesta = clienteSocket.recv(1024)
 print("Respuesta:\n" + str(mensajeRespuesta, "utf-8"))
 clienteSocket.close()
+
+# Debitar 2100 (saldo inicial 2000)
+clienteSocket = socket(AF_INET, SOCK_STREAM)
+clienteSocket.connect((servidorNombre,servidorPuerto))
+mensaje = "debitar 2100"
+clienteSocket.send(bytes(mensaje, "utf-8"))
+mensajeRespuesta = clienteSocket.recv(1024)
+print("Respuesta:\n" + str(mensajeRespuesta, "utf-8"))
+clienteSocket.close()
+
+# Debitar 100 
+clienteSocket = socket(AF_INET, SOCK_STREAM)
+clienteSocket.connect((servidorNombre,servidorPuerto))
+mensaje = "debitar 100"
+clienteSocket.send(bytes(mensaje, "utf-8"))
+mensajeRespuesta = clienteSocket.recv(1024)
+print("Respuesta:\n" + str(mensajeRespuesta, "utf-8"))
+clienteSocket.close()
+
+# Solicitar saldo
+clienteSocket = socket(AF_INET, SOCK_STREAM)
+clienteSocket.connect((servidorNombre,servidorPuerto))
+mensaje = "saldo"
+clienteSocket.send(bytes(mensaje, "utf-8"))
+mensajeRespuesta = clienteSocket.recv(1024)
+print("Respuesta:\n" + str(mensajeRespuesta, "utf-8"))
+clienteSocket.close()
+
+# Acreditar saldo, el doble
+clienteSocket = socket(AF_INET, SOCK_STREAM)
+clienteSocket.connect((servidorNombre,servidorPuerto))
+mensaje = "acreditar "+str(int(mensajeRespuesta[13:]))
+clienteSocket.send(bytes(mensaje, "utf-8"))
+mensajeRespuesta = clienteSocket.recv(1024)
+print("Respuesta:\n" + str(mensajeRespuesta, "utf-8"))
+clienteSocket.close()
+
+# Solicitar saldo
+clienteSocket = socket(AF_INET, SOCK_STREAM)
+clienteSocket.connect((servidorNombre,servidorPuerto))
+mensaje = "saldo"
+clienteSocket.send(bytes(mensaje, "utf-8"))
+mensajeRespuesta = clienteSocket.recv(1024)
+print("Respuesta:\n" + str(mensajeRespuesta, "utf-8"))
+clienteSocket.close()
+
